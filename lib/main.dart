626c97edbf199afed5baee87b98b42b38993bf6b
unknown <ykmhabib97@gmail.com>
2023-11-13 20:21:37 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// View imports
+import 'package:notes_app/views/notes_view.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: const NotesView(),
+    );
+  }
+}
