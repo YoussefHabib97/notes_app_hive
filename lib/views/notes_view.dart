91ad388160a8c504b2f57180375b4617b7df357a
unknown <ykmhabib97@gmail.com>
2023-11-13 20:46:07 +0200

    Built a custom app bar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:notes_app/views/widgets/notes_view_body.dart';
+
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: const NotesViewBody(),
+    );
   }
 }
---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'custom_icon_button.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Text(
+          "Notes",
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+        Spacer(),
+        CustomIconButton(),
+      ],
+    );
+  }
+}
