51767369386ce28cd0ece987e1ac85047288463d
unknown <ykmhabib97@gmail.com>
2023-11-30 10:48:31 +0200

    Created edit note

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,63 +1,73 @@
 import 'package:flutter/material.dart';
 
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        left: 16,
-      ),
-      decoration: BoxDecoration(
-        color: const Color(0xFFFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              "Flutter Tips",
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.black,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const EditNoteView(),
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
+        ),
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                "Flutter Tips",
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  "Build your career with Tharwat Samy",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                icon: const FaIcon(
+                  FontAwesomeIcons.trash,
+                  size: 24,
+                  color: Colors.black,
+                ),
+                onPressed: () {},
               ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
               child: Text(
-                "Build your career with Tharwat Samy",
+                "November 14, 2023",
                 style: TextStyle(
                   fontSize: 16,
                   color: Colors.black.withOpacity(0.5),
                 ),
               ),
-            ),
-            trailing: IconButton(
-              icon: const FaIcon(
-                FontAwesomeIcons.trash,
-                size: 24,
-                color: Colors.black,
-              ),
-              onPressed: () {},
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              "November 14, 2023",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black.withOpacity(0.5),
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
