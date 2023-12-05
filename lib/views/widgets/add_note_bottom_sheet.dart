76efd77f2336e38ad8d7793ec2bfa35cf80b9440
Youssef Habib <ykmhabib97@gmail.com>
2023-12-05 02:47:47 +0200

    Added user input & validation for it

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// Widget imports
+import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -12,15 +14,60 @@ class AddNoteBottomSheet extends StatelessWidget {
         horizontal: 16,
         vertical: 32,
       ),
+      child: SingleChildScrollView(
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      autovalidateMode: autovalidateMode,
       child: Column(
         children: [
-          CustomTextField(hintText: "Title"),
-          SizedBox(
-            height: 32,
+          CustomTextField(
+            hintText: "Title",
+            onSaved: (value) {
+              title = value;
+            },
+          ),
+          const SizedBox(
+            height: 16,
           ),
           CustomTextField(
+            onSaved: (value) {
+              description = value;
+            },
             hintText: "Description",
             maxLines: 5,
+          ),
+          const SizedBox(height: 16),
+          CustomButton(
+            onTap: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
           ),
         ],
       ),
